bca488d3e316d756a31f01f22eb20bf89455d84d
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-05 14:38:12 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,5 +1,6 @@
 -- Purpose: Retrieve the total number of bookings made by each user using COUNT and GROUP BY
--- Description: This query finds the total number of bookings made by each user. It uses a JOIN to link users with their bookings and then uses the COUNT function along with a GROUP BY clause to count the bookings for each distinct user.
+-- Description: This query finds the total number of bookings made by each user. 
+-- It uses a JOIN to link users with their bookings and then uses the COUNT function along with a GROUP BY clause to count the bookings for each distinct user.
 SELECT 
     u.user_id,
     u.first_name,
@@ -14,7 +15,8 @@ ORDER BY total_bookings DESC;
 
 
 -- Purpose: Rank properties based on total number of bookings using window functions
--- Description: This query ranks properties based on the total number of bookings they have received. It uses a Common Table Expression (CTE) to first count the bookings per property, and then applies a window function to rank the properties based on that count.
+-- Description: This query ranks properties based on the total number of bookings they have received. 
+-- It uses a Common Table Expression (CTE) to first count the bookings per property, and then applies a window function to rank the properties based on that count.
 WITH property_booking_counts AS (
     SELECT 
         p.property_id,
