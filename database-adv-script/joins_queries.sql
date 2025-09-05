e1a36adb5bb0f408be27d4abec8db7140f74f770
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-05 13:44:24 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,7 @@
--- Purpose: Fetch all bookings with user information
--- Description: This query retrieves all booking records and joins them with user information.
+-- INNER JOIN QUERY
+-- - Purpose: Fetch all bookings with user information
+-- - Description: This query retrieves all booking records and joins them with user information.
+-- - Use Case:
 SELECT
     b.booking_id,
     b.property_id,
@@ -17,9 +19,10 @@ FROM
 INNER JOIN
     "user" AS u ON b.user_id = u.user_id;
 
-
--- Purpose: Retrieve all properties and their reviews, including properties with no reviews
--- Description: This query retrieves all property records and joins them with their reviews.
+-- LEFT JOIN QUERY
+-- - Purpose: Retrieve all properties and their reviews, including properties with no reviews
+-- - Description: This query retrieves all property records and joins them with their reviews.
+-- - Use Case:
 SELECT
     p.property_id,
     p.name AS property_name,
@@ -34,9 +37,10 @@ FROM
 LEFT JOIN
     review AS r ON p.property_id = r.property_id;
 
-
--- Purpose: Retrieve all users and all bookings, including users without bookings and bookings without users
--- Description: This query uses a FULL OUTER JOIN to retrieve all records from both the "user" and "booking" tables.
+-- OUTER JOIN QUERY
+-- - Purpose: Retrieve all users and all bookings, including users without bookings and bookings without users
+-- - Description: This query uses a FULL OUTER JOIN to retrieve all records from both the "user" and "booking" tables.
+-- - Use Case:
 SELECT
     u.user_id,
     u.first_name,
